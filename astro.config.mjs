240c6d1b8e0b283c83cd2eb6f212e2b47033bcce
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-07-08 11:34:18 +0530

    main-react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://swastikdan.in',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), react()]
 });
--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,32 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import eslintPluginAstro from 'eslint-plugin-astro';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
   ...eslintPluginAstro.configs.recommended,
   { parserOptions: { ecmaVersion: 'latest', sourceType: 'module' } },
-  { rules: { semi: ['error', 'always'], quotes: ['error', 'double', { allowTemplateLiterals: true }], '@typescript-eslint/triple-slash-reference': 'off' } },
+  {
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'double', { allowTemplateLiterals: true }],
+      '@typescript-eslint/triple-slash-reference': 'off'
+    }
+  },
   {
     overrides: [
       {
         files: ['*.astro'],
         parser: 'astro-eslint-parser',
         parserOptions: { parser: '@typescript-eslint/parser', extraFileExtensions: ['.astro'] },
-        rules: {},
-      },
-    ],
-  },
+        rules: {}
+      }
+    ]
+  }
 ];
-

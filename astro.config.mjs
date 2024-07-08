9f412e4fa272b4134bcb3bcbd2426762c6629c16
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-07-08 11:10:00 +0530

    main-vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://swastikdan.in',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });

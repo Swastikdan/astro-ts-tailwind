cc4af5be4b6b21c7d9a89a0b359b03a97867f99a
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-07-08 11:11:33 +0530

    main-node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://swastikdan.in',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
